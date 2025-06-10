31e8d3f2c68c89ec091ffd84e48501c6080b3459
Ibrahim-252 <studentofhalic@gmail.com>
2025-06-10 14:37:16 +0300

    Exercise 1 completed

---
+++ Exercise1/main.js
@@ -0,0 +1,21 @@
+let x = 7;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+
+console.log(x/y);
+console.log(x % y);
+
+// pre increment
+// console.log(++a)
+// console.log("pre increment")
+// console.log(a)
+// post increment
+console.log("post increment")
+console.log(a++)
+
+console.log(a)
+
+console.log(--b)
