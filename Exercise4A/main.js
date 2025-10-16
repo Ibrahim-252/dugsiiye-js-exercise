a6f1da66b0d2a662a85787130a9e95c9bb0f4838
Ibrahim-252 <studentofhalic@gmail.com>
2025-10-16 08:36:51 +0300

    Exercise 4A completed

---
+++ Exercise4A/main.js
@@ -0,0 +1,16 @@
+// const salaan = (name)=> {
+//     console.log("hello" + name)
+// }
+
+// salaan("Ali");
+
+function add( a, b) {
+    return a + b;
+}
+
+console.log(add(5,4));
+console.log(add(10,20));
+console.log(add(6,4));
+
+
+
