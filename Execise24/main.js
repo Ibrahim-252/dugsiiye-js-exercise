c381bb8a2979587eee2c8d50aadbb1a80af33f98
Ibrahim-252 <studentofhalic@gmail.com>
2025-12-05 00:37:04 +0300

    Add Exercise24

---
+++ Execise24/main.js
@@ -0,0 +1,15 @@
+
+// //Default Parameters
+// function greet (name="Guest") {
+// console.log(`Hello ${name}`)
+// }
+
+// greet("Deeqa")
+
+
+function calculateArea(width, height, width) {  
+  return width + height;
+}
+
+console.log(calculateArea(4));      
+console.log(calculateArea(4, 6));   
