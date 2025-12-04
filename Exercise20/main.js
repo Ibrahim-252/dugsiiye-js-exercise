8ae310661d1cfa5003900cd832d7d7829959be71
Ibrahim-252 <studentofhalic@gmail.com>
2025-12-04 16:59:05 +0300

    Added Exercise20

---
+++ Exercise20/main.js
@@ -0,0 +1,16 @@
+
+// const numbers = [1, 2, 3, 4];
+// const squaredNumbers = numbers.map(num => num * 2);
+// console.log(squaredNumbers); // Output: [2, 4, 6, 8]
+
+// const fruit = ["Appple", "Cherry", "banana"];
+
+// const lengths = fruit.map(fruit => fruitlength);
+
+// console.log(lengths)
+const fruits = ["apple", "banana", "cherry"];
+
+const lengths = fruits.map(fruit => fruit.length);
+
+console.log(lengths);  
+// Output: [5, 6, 6]
